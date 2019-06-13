81844759379ca17a90f0429d25c32a2f38a471a8
felix-me <felixm.manchester@gmail.com>
2019-06-13 17:50:27 -0400

    Added tests again and fixed path issue

---
+++ carparkpractice/src/main/java/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+public class App {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Application does not take any arguments, Exiting...");
+            System.exit(0);
+        }
+    }
+}
