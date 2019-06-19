baf0270f1d3e1359c7948975dbc33b3329bf9653
felix-me <felixmelligan@hotmail.co.uk>
2019-06-19 17:28:55 -0400

    class tested, code coverage 100%

--- carparkpractice/src/test/java/app/car_park/machines/MachineTest.java
+++ carparkpractice/src/test/java/app/car_park/machines/MachineTest.java
@@ -1,9 +1,11 @@
 package app.car_park.machines;
 
-import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import utils.State;
+
+import static org.junit.Assert.assertEquals;
 
 public class MachineTest {
     private Machine m;
@@ -12,15 +14,55 @@ public class MachineTest {
     public void setUp() {
         m = Mockito.mock(
                 Machine.class,
-                Mockito.CALLS_REAL_METHODS);
+                Mockito.withSettings().useConstructor().defaultAnswer(Mockito.CALLS_REAL_METHODS));
     }
 
     @Test
     public void machineClassHasIncrementingId() {
         Machine m2 = Mockito.mock(
                 Machine.class,
-                Mockito.CALLS_REAL_METHODS);
-        assertEquals(0, m.getId());
-        assertEquals(1, m2.getId());
+                Mockito.withSettings().useConstructor().defaultAnswer(Mockito.CALLS_REAL_METHODS));
+        int mId = m.getId();
+        assertEquals(++mId, m2.getId());
+    }
+
+    @Test
+    public void hasEnumContainingStateSetToOFFAfterConstructor() {
+        assertEquals(State.OFF, m.getState());
+    }
+
+    @Test
+    public void canSwitchMachineOnIfOFF() {
+        assertEquals(State.OFF, m.getState());
+
+        m.switchOn();
+
+        assertEquals(State.ON, m.getState());
+    }
+
+    @Test
+    public void switchOnDoesNothingIfStateAlreadyON() {
+        m.switchOn();
+        assertEquals(State.ON, m.getState());
+
+        m.switchOn();
+        assertEquals(State.ON, m.getState());
+    }
+
+    @Test
+    public void canSwitchStateToOFFIfON() {
+        m.switchOn();
+        assertEquals(State.ON, m.getState());
+
+        m.switchOff();
+        assertEquals(State.OFF, m.getState());
+    }
+
+    @Test
+    public void switchOffDoesNothingIfStateAlreadyOFF() {
+        assertEquals(State.OFF, m.getState());
+
+        m.switchOff();
+        assertEquals(State.OFF, m.getState());
     }
 }
